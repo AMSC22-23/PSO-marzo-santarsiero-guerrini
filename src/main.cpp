b2dc5566a1e0cfc30f2c2a3df5a854f8bb2e0698
ague01 <aguerrini01@gmail.com>
2023-11-28 12:26:33 +0100

    main updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "../include/ParticleSwarmOptimization.hpp"
+#include "../include/TestFunctions.hpp"
+#include <iostream>
+
 // hello world main
 int main(int argc, char** argv) {
-	Particle(2, false, [](Vector v) { return v[0] * v[0] + v[1] * v[1]; });
-	std::cout << "Hello World!" << std::endl;
+	// Sphere function optimization
+	std::cout << "Sphere optimization:" << std::endl;
+	std::pair<double, double> bounds = TestFunctions::get_bounds("sphere");
+	ParticleSwarmOptimization pso(TestFunctions::sphere, 50, 2, bounds.first, bounds.second, 1000, 10e-6, 0.4, 0.9, 2.0);
+	pso.initialize();
+	int status = pso.optimize();
+	std::cout << "Status: " << status << std::endl;
+	std::cout << "Global best value: " << pso.get_global_best_value() << std::endl;
+	std::cout << "Global best position: (" << pso.get_global_best_position()[0] << ", " << pso.get_global_best_position()[1] << ")" << std::endl;
+	std::cout << "Iterations: " << pso.get_iter() << std::endl;
+	std::cout << "Tolerance: " << pso.get_tolerance() << std::endl;
+	std::cout << std::endl;
 	return 0;
 }
