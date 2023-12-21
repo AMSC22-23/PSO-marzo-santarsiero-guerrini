fe837a6252476ac91716a98f87e81f681e0bc8a9
ague01 <aguerrini01@gmail.com>
2023-12-21 11:22:07 +0100

    docs updated

--- docs/index.js
+++ docs/index.js
@@ -4,5 +4,6 @@ var index =
     [ "Requirements", "index.html#requirements", null ],
     [ "How to compile and run", "index.html#how-to-compile-and-run", null ],
     [ "Available tests", "index.html#available-tests", null ],
+    [ "Documentation", "index.html#documentation", null ],
     [ "Group members", "index.html#group-members", null ]
 ];
--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['description_0',['Description',['../index.html#description',1,'']]]
+  ['description_0',['Description',['../index.html#description',1,'']]],
+  ['documentation_1',['Documentation',['../index.html#documentation',1,'']]]
 ];
